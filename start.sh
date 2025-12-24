8174bf04fc831fcbf92fc14e2128b0b43076277b
serumwriter <60053691+serumwriter@users.noreply.github.com>
2025-12-24 14:31:57 -0700

    Set PYTHONPATH for agencyvault_app

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-exec uvicorn agencyvault_app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
+export PYTHONPATH=/opt/render/project/src
 
+exec uvicorn agencyvault_app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
