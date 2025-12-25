aa08fae237c5ff95764261375f79fc9c176979c4
serumwriter <60053691+serumwriter@users.noreply.github.com>
2025-12-24 20:11:00 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ set -o pipefail
 
 export PYTHONPATH=/opt/render/project/src
 
-exec uvicorn agencyvault_app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
+exec uvicorn main:app --host 0.0.0.0 --port "${PORT:-8000}"
+
